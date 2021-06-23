71792afb0ad62efbf28daa4fab3c95ed1dc5920f
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2021-06-23 18:34:12 +0500

    Last Commit

--- src/main/java/com/kamronbek/jwt/config/SecurityConfig.java
+++ src/main/java/com/kamronbek/jwt/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Disabling HTTP BASIC Authentication
+        // HTTP BASIC Authentication
         http.httpBasic();
 
         // Disabling Session Management
